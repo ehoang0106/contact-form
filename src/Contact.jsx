c7eb346b8862917d133b7b6abd7a097118d70c29
Khoa Hoang <khoahoang1698@gmail.com>
2024-03-15 10:00:56 -0700

    update

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -35,14 +35,14 @@ const Contact = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                             <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
                         </svg>
-                        <a href="https://www.facebook.com/smarternutrition1/" target="_blank">Facebook</a>
+                        <a href="https://www.facebook.com/smarternutrition1/" target="_blank" rel="noopener noreferrer">Facebook</a>
                     </li>
                     <li>
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-amazon" viewBox="0 0 16 16">
                             <path d="M10.813 11.968c.157.083.36.074.5-.05l.005.005a90 90 0 0 1 1.623-1.405c.173-.143.143-.372.006-.563l-.125-.17c-.345-.465-.673-.906-.673-1.791v-3.3l.001-.335c.008-1.265.014-2.421-.933-3.305C10.404.274 9.06 0 8.03 0 6.017 0 3.77.75 3.296 3.24c-.047.264.143.404.316.443l2.054.22c.19-.009.33-.196.366-.387.176-.857.896-1.271 1.703-1.271.435 0 .929.16 1.188.55.264.39.26.91.257 1.376v.432q-.3.033-.621.065c-1.113.114-2.397.246-3.36.67C3.873 5.91 2.94 7.08 2.94 8.798c0 2.2 1.387 3.298 3.168 3.298 1.506 0 2.328-.354 3.489-1.54l.167.246c.274.405.456.675 1.047 1.166ZM6.03 8.431C6.03 6.627 7.647 6.3 9.177 6.3v.57c.001.776.002 1.434-.396 2.133-.336.595-.87.961-1.465.961-.812 0-1.286-.619-1.286-1.533M.435 12.174c2.629 1.603 6.698 4.084 13.183.997.28-.116.475.078.199.431C13.538 13.96 11.312 16 7.57 16 3.832 16 .968 13.446.094 12.386c-.24-.275.036-.4.199-.299z" />
                             <path d="M13.828 11.943c.567-.07 1.468-.027 1.645.204.135.176-.004.966-.233 1.533-.23.563-.572.961-.762 1.115s-.333.094-.23-.137c.105-.23.684-1.663.455-1.963-.213-.278-1.177-.177-1.625-.13l-.09.009q-.142.013-.233.024c-.193.021-.245.027-.274-.032-.074-.209.779-.556 1.347-.623" />
                         </svg>
-                        <a href="https://www.amazon.com/stores/node/18010893011" target="_blank">We are on Amazon</a>
+                        <a href="https://www.amazon.com/stores/node/18010893011" target="_blank" rel="noopener noreferrer">We are on Amazon</a>
                     </li>
                     <li>
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
@@ -62,7 +62,6 @@ const Contact = () => {
                     <input type="text" placeholder="Name..." />
                     <input type="email" placeholder="Email..." />
                     <input type="number" placeholder="Phone number..." />
-                    {/* <input className="message" type="text" placeholder="Message..."/> */}
                     <textarea name="message" id="contact-form-message" cols="5" rows="4" placeholder="Message..."></textarea>
 
                     <button>SEND MESSAGE</button>
